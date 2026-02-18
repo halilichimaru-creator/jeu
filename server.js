214cc234dd37b450fb6d8cbfed613ae6708328e5
laskdevv-collab <laskdevv@gmail.com>
2026-02-18 22:43:22 +0100

    Fix Vercel routing: explicitly handle root route

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname)));
 
 const players = {};
 
